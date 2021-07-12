0a944406804c2257fbe150975e1c9bf9b7ab537a
Saf <saf@irkenkitties.com>
2021-07-12 00:28:29 -0700

    Renaming some things

--- lib/cryptopals/crack_cyclic_xor.rb
+++ lib/cryptopals/crack_cyclic_xor.rb
@@ -9,8 +9,8 @@ module Cryptopals
     def call(ciphertext)
       key_size = CyclicXorKeysize.call(ciphertext).first
 
-      key = transpose_blocks(ciphertext, key_size).map do |single_xor|
-        SingleByteXor.crack(single_xor.bytes2hex).key.chr
+      key = transpose_to_columns(ciphertext, key_size).map do |single_byte_xor|
+        SingleByteXor.crack(single_byte_xor.bytes2hex).key.chr
       end.join
 
       ciphertext.cyclic_xor(key)
@@ -18,16 +18,16 @@ module Cryptopals
 
     private
 
-    def transpose_blocks(string, size)
-      columns = []
-      string.bytes.each_slice(size) do |slice|
-        slice += [0] * (size - slice.size)
-        slice.each_with_index do |byte, i|
-          columns[i] ||= ''
-          columns[i] += byte.chr
+    def transpose_to_columns(string, size)
+      [].tap do |columns|
+        string.bytes.each_slice(size) do |slice|
+          slice += [0] * (size - slice.size)
+          slice.each_with_index do |byte, i|
+            columns[i] ||= ''
+            columns[i] += byte.chr
+          end
         end
       end
-      columns
     end
   end
 end
