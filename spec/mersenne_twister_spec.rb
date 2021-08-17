7a86b3ca225518dcae06806e174bcd62e0747c4f
Saf <saf@irkenkitties.com>
2021-08-16 18:03:43 -0700

    Renaming stuff

--- spec/mersenne_twister_spec.rb
+++ spec/mersenne_twister_spec.rb
@@ -1,21 +1,24 @@
 # frozen_string_literal: true
 
 RSpec.describe(Cryptopals::MersenneTwister) do
-  describe '#genrand_int32' do
+  describe '#rand_int32' do
+    let(:seed) { described_class::DEFAULT_SEED }
     let(:mt19937) { described_class.new }
+    let(:uint32_max) { 2**32 - 1 }
 
-    context 'when compared with the Ruby mt19937 generator' do
-      let(:ruby_mt19937) do
-        Random.srand(5489)
-        1000.times.map { Random.rand(2**32 - 1) }
+    context 'when compared with the Ruby mt19937 generator with same seed' do
+      let(:ruby_rand) do
+        Random.srand(seed)
+        1000.times.map { Random.rand(uint32_max) }
       end
 
-      let(:my_mt19937) do
+      let(:my_rand) do
+        mt19937.srand(seed)
         1000.times.map { mt19937.rand_uint32 }
       end
 
-      it 'generates the same out put as Random.rand' do
-        expect(my_mt19937).to eq(ruby_mt19937)
+      it 'generates the same output as Random.rand' do
+        expect(my_rand).to eq(ruby_rand)
       end
     end
   end
